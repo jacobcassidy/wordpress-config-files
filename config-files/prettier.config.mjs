2c12d34e4a774e33dbe4af2205f84da260714237
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-15 16:58:09 +0700

    Add `config-files`

---
+++ config-files/prettier.config.mjs
@@ -0,0 +1,22 @@
+/**
+ * Prettier configuration.
+ * @see https://prettier.io/docs/en/configuration.html
+ */
+import wpConfig from '@wordpress/prettier-config';
+
+const config = {
+	...wpConfig,
+	plugins: [ 'prettier-plugin-multiline-arrays' ],
+	overrides: [
+		...wpConfig.overrides,
+		{
+			files: '*.json',
+			options: {
+				multilineArraysWrapThreshold: 0,
+			},
+		},
+	],
+	printWidth: 120,
+};
+
+export default config;
