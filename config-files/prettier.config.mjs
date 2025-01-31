b57ebced9907e4dd114a1983f7e91a3d727eec40
Jacob Cassidy <jacob@jacobcassidy.com>
2025-01-31 16:09:21 +0700

    Remove `prettier-plugin-multiline-arrays`

--- config-files/prettier.config.mjs
+++ config-files/prettier.config.mjs
@@ -6,16 +6,6 @@ import wpConfig from '@wordpress/prettier-config';
 
 const config = {
 	...wpConfig,
-	// plugins: [ 'prettier-plugin-multiline-arrays' ],
-	// overrides: [
-	// 	...wpConfig.overrides,
-	// 	{
-	// 		files: '*.json',
-	// 		options: {
-	// 			multilineArraysWrapThreshold: 0,
-	// 		},
-	// 	},
-	// ],
 	printWidth: 120,
 };
 
